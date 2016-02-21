808e6e9e4478d61ff83f1d1844d20ac43b4201c4
guoxuri <853523391@qq.com>
2016-02-22 00:27:39 +0800

    closed #4

---
+++ DesignPattern_Calculator/DesignPattern_Calculator/OperationAdd.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPattern_Calculator
+{
+    public class OperationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA + NumberB;
+            return result;
+        }
+    }
+}
---
+++ DesignPattern_Calculator/DesignPattern_Calculator/OperationSub.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPattern_Calculator
+{
+    public class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA - NumberB;
+            return result;
+        }
+    }
+}
