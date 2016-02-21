3884a8a372b871c311ff622057f257d69717c9f8
guoxuri <853523391@qq.com>
2016-02-22 00:41:43 +0800

    close #6

---
+++ DesignPattern_Calculator/OperationLibrary/Operation.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace DesignPattern_Calculator
+{
+    public class Operation
+    {
+        private double _numberA = 0;
+        private double _numberB = 0;
+
+        public double NumberA
+        {
+            get
+            {
+                return _numberA;
+            }
+
+            set
+            {
+                _numberA = value;
+            }
+        }
+
+        public double NumberB
+        {
+            get
+            {
+                return _numberB;
+            }
+
+            set
+            {
+                _numberB = value;
+            }
+        }
+
+        public virtual double GetResult()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+    public class OperationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA + NumberB;
+            return result;
+        }
+    }
+    public class OperationDiv : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            if (NumberB == 0)
+            {
+                throw new Exception("除数不能为0.");
+            }
+            result = NumberA / NumberB;
+            return result;
+        }
+    }
+    public class OperationMul : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA * NumberB;
+            return result;
+        }
+    }
+    public class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA - NumberB;
+            return result;
+        }
+    }
+    public class OperationFactory
+    {
+        public static Operation createOperate(string operate)
+        {
+            Operation oper = null;
+            switch (operate)
+            {
+                case "+":
+                    oper = new OperationAdd();
+                    break;
+                case "-":
+                    oper = new OperationSub();
+                    break;
+                case "*":
+                    oper = new OperationMul();
+                    break;
+                case "/":
+                    oper = new OperationDiv();
+                    break;
+                default:
+                    break;
+            }
+            return oper;
+        }
+    }
+}
